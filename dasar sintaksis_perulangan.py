5045f718e69f9725555e40a308d653c228bd8e49
RemoteWorkerIDN <danahiswara.danas@gmail.com>
2022-07-24 00:19:36 +0700

    dasar perulangan

--- dasar sintaksis_perulangan.py
+++ dasar sintaksis_perulangan.py
@@ -5,3 +5,7 @@ print('Ibu berkata, "baca semua buku-bukumu')
 book_count = 10
 jumlah_buku_sudah_dibaca = 0
 print('baik ibu')
+
+for jumlah_buku_sudah_dibaca in range (book_count + 1):
+    print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+
