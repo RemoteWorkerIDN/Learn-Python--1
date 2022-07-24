36426fafdf2722737cc0f6497f377c0becd11d90
RemoteWorkerIDN <danahiswara.danas@gmail.com>
2022-07-24 23:06:00 +0700

    perulangan dengan while dengan jumlah tak terbatas dan ada variabel pemberhenti

--- dasar sintaksis_perulangan_while.py
+++ dasar sintaksis_perulangan_while.py
@@ -2,13 +2,21 @@
 perulangan pada python
 """
 print('Ibu berkata, "baca semua buku-bukumu')
-book_count = 1000
-jumlah_buku_sudah_dibaca = 0
+book_count = 10
+jumlah_buku_sudah_dibaca_dan_dipahami = 0
+total_jumlah_baca_buku = 0
 print('baik ibu')
 
-while jumlah_buku_sudah_dibaca < book_count:
-    jumlah_buku_sudah_dibaca = jumlah_buku_sudah_dibaca + 1
-    print(f'buku ke {jumlah_buku_sudah_dibaca} sudah dibaca')
+while total_jumlah_baca_buku < book_count * 2:
+    total_jumlah_baca_buku = total_jumlah_baca_buku + 1
+    if jumlah_buku_sudah_dibaca_dan_dipahami == 9:
+        print(f' buku ke {jumlah_buku_sudah_dibaca_dan_dipahami + 1} belum dipahami')
+    else:
+        jumlah_buku_sudah_dibaca_dan_dipahami = jumlah_buku_sudah_dibaca_dan_dipahami + 1
+        print(f'buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} sudah dibaca')
 
-
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+if jumlah_buku_sudah_dibaca_dan_dipahami < 10:
+    print(f' ibu aku baru membaca dan memahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
+else:
+    print(f'ibu aku sudah membaca dan memahami semua bukuku')
+#print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
