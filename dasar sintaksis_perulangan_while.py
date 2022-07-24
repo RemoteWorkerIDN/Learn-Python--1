5584f74e26d8c04338cc52f513bbc80591e6a649
RemoteWorkerIDN <danahiswara.danas@gmail.com>
2022-07-24 23:08:19 +0700

    rename in english

--- dasar sintaksis_perulangan_while.py
+++ dasar sintaksis_perulangan_while.py
@@ -3,20 +3,20 @@ perulangan pada python
 """
 print('Ibu berkata, "baca semua buku-bukumu')
 book_count = 10
-jumlah_buku_sudah_dibaca_dan_dipahami = 0
-total_jumlah_baca_buku = 0
+understood_count = 0
+read_count = 0
 print('baik ibu')
 
-while total_jumlah_baca_buku < book_count * 2:
-    total_jumlah_baca_buku = total_jumlah_baca_buku + 1
-    if jumlah_buku_sudah_dibaca_dan_dipahami == 9:
-        print(f' buku ke {jumlah_buku_sudah_dibaca_dan_dipahami + 1} belum dipahami')
+while read_count < book_count * 2:
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f' buku ke {understood_count + 1} belum dipahami')
     else:
-        jumlah_buku_sudah_dibaca_dan_dipahami = jumlah_buku_sudah_dibaca_dan_dipahami + 1
-        print(f'buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} sudah dibaca')
+        understood_count = understood_count + 1
+        print(f'buku ke {understood_count} sudah dibaca')
 
-if jumlah_buku_sudah_dibaca_dan_dipahami < 10:
-    print(f' ibu aku baru membaca dan memahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
+if understood_count < 10:
+    print(f' ibu aku baru membaca dan memahami {understood_count}')
 else:
     print(f'ibu aku sudah membaca dan memahami semua bukuku')
 #print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
