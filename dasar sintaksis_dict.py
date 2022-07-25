6db1bfe521528b5bb2e61e305010a4b485f22d9c
RemoteWorkerIDN <danahiswara.danas@gmail.com>
2022-07-25 23:09:59 +0700

    convert dict = JSON

--- dasar sintaksis_dict.py
+++ dasar sintaksis_dict.py
@@ -18,3 +18,23 @@ print(users["email"])
 print(users["email"])
 print(users["address"])
 print(users["address"]["geo"]["lat"])
+
+"""
+Cara merubah dictionary menjadi JSON
+"""
+# untuk merubah dict menjadi json harus import package JSON yang ada pada python
+print(users)
+
+print("\nUbah dict ke JSON")
+# hasil dari json.dumps adalah string
+import json
+#result = json.dumps(users)
+#print(result)
+
+result = json.dump
+print((type)(result))
+print(result)
+
+with open("result.json","w") as file:
+    json.dump(users, file)
+
